d342afe2b58efbd3dc93d1d31aea17428b5b1c0d
ZebornDuan <rebornduan@qq.com>
2018-04-25 14:52:09 +0800

    process

--- CacheReplacement/process.py
+++ CacheReplacement/process.py
@@ -9,12 +9,17 @@ result.close()
 
 g = open('readme.md', 'w')
 g.write('| **Program** | **LRU_time** | **LRU_CPI** | **LRU_miss** | \
-**Random_time** | **Random_CPI** | **DRRIP_miss** | \
-**DRRIP_time** | **DRRIP_CPI** | **DRRIP_miss** |\n\
+**Random_time** | **Random_CPI** | **Random_miss** | \
+**DRRIP_time** | **DRRIP_CPI** | **DRRIP_miss** | \
+**least_time** | **least_CPI** | **least_miss** |\n\
 | ----: | -------: | ----: | ----: | \
+-------: | ----: | ----: | \
 -------: | ----: | ----: | \
 -------: | ----: | ----: |\n')
 
+algorithm = ['LRU', 'Random', 'DRRIP']
+m3 = [0] * 3
+c3 = [0] * 3
 for (i, file) in enumerate(glob.glob(os.getcwd().replace('\\', '/') + '/runs/*')):
 	with gzip.open(file, 'r') as f:
 		if i % 3 == 0:
@@ -22,14 +27,25 @@ for (i, file) in enumerate(glob.glob(os.getcwd().replace('\\', '/') + '/runs/*')
 		lines = f.read().decode('utf-8').split('\n')
 		g.write(' | ' + t[3 * i].replace('\n', '').split(' ')[-1])
 		try:
-			g.write(' | ' + lines[-44].split(' ')[-7])
+			cpi = lines[-44].split(' ')[-7]
 		except:
-			g.write(' | ' + lines[-39].split(' ')[-7])
-		g.write(' | ' + lines[-6].split(' ')[-1])
-		# print(i)
-		# print(t[3 * i].replace('\n', '').split(' ')[-1])
-		# print(lines[-6].split(' ')[-1])
-		# print(lines[-44].split(' ')[-7])
+			cpi = lines[-39].split(' ')[-7]
+		g.write(' | ' + cpi)
+		c3[i % 3] = float(cpi)
+		miss = lines[-6].split(' ')[-1]
+		g.write(' | ' + miss)
+		m3[i % 3] = float(miss)
 		if i % 3 == 2:
+			t3 = [float(t[3 * (i - 2 + j) ].replace('\n', '').split(' ')[-1]) for j in range(3)]
+			print(t3)
+			g.write(' | ' + algorithm[t3.index(min(t3[0], t3[1], t3[2]))])
+			if not m3[0] == m3[1] == m3[2]:
+				g.write(' | ' + algorithm[c3.index(min(c3[0], t3[1], c3[2]))])
+			else:
+				g.write(' | --')
+			if not m3[0] == m3[1] == m3[2]:
+				g.write(' | ' + algorithm[m3.index(min(m3[0], m3[1], m3[2]))])
+			else:
+				g.write(' | --')
 			g.write(' |\n')
 
